d0188fc524a00f25f4b870b3741fe0dd37ec62cf
Roman Dearing <romandearing@Romans-MacBook-Air.local>
2025-02-06 16:58:00 -0500

    additional code

--- script.js
+++ script.js
@@ -6,5 +6,15 @@ keys.forEach((key) => {
 
 function playNote(key) {
   const noteAudio = document.getElementById(key.dataset.note);
+
+noteAudio.currentTime = 0 
+
   noteAudio.play();
+
+  key.classList.add('active');
+
+  noteAudio.addEventListener('ended', () => {
+    key.classList.remove('active');
+  });
+
 }
